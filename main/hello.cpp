da4b83496504f6febc4b87b6dc642a0923668679
ShubhamKarjini <113159992+ShubhamKarjini@users.noreply.github.com>
2023-02-17 19:52:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "gg well played\n";
+  cout << "changes are being done\n";
   return 0;
 }
